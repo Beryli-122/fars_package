54c1971bd06c51d14d7a702033f046b4dec0d2bd
Borui Li <boruili@BoruideMacBook-Pro.local>
2020-07-07 21:56:12 -0400

    debug

--- tests/testthat/test-testfars.R
+++ tests/testthat/test-testfars.R
@@ -5,7 +5,5 @@ library(maps)
 library(fars)
 
 test_that("fars_map_state() works correctly", {
-  expect_silent(fars_map_state(12, 2013))
-  expect_silent(fars_map_state(16, 2014))
   expect_error(fars_map_state(17, 2018))
 })
